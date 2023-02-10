d46d5eba804352f0e24876ffcf3d5043f1f46e2b
seanj06 <seanj06@hotmail.com>
2023-02-10 22:01:27 +0000

    Add first database model

--- main/models.py
+++ main/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from datetime import date
 
 # Create your models here.
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    quantity = models.PositiveIntegerField(null=False, default=0)
+    calories = models.FloatField(null=False, default=0)
+
+    def __str__(self):
+        return self.name
--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, view
+from .models import Food
 
 # Create your views here.
+
+
+class FoodList(generic.ListView):
+    model = Food
+    template_name = 'base.html'
